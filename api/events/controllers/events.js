dc2cd396905c84cd3c819a329ea88c2562449c85
Anastasiia Bardyuzha <anastasiia.bardyuzha@extrawest.com>
2021-06-11 16:26:19 +0300

    added AccessPolicy for create, update & delete evt

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,5 +1,5 @@
 'use strict';
-const { sanitizeEntity } = require('strapi-utils')
+const { sanitizeEntity, parseMultipartData } = require('strapi-utils')
 
 const { default: createStrapi } = require("strapi");
 
@@ -9,7 +9,72 @@ const { default: createStrapi } = require("strapi");
  */
 
 module.exports = {
+  // Create events with owner user only
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(data, { files });
+    } else {
+      ctx.request.body.author = ctx.state.user.id;
+      entity = await strapi.services.events.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
+  // Update events with owner user only
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      'user.id': ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.events.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.events.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
+  // Delete events with owner user only
+
+  async create(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [events] = await strapi.services.events.find({
+      id: ctx.params.id,
+      'user.id': ctx.state.user.id,
+    });
+
+    if (!events) {
+      return ctx.unauthorized(`You can't delete this entry`);
+    }
+
+    entity = await strapi.services.events.delete({ id });
+
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
+
   //  Get Logged in users 
+
   async me(ctx) {
     const user = ctx.state.user;
 

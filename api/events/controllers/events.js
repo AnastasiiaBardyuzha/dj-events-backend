13885ffd9b6168444eaf93f6d98c57ee0bfd1c16
Anastasiia Bardyuzha <anastasiia.bardyuzha@extrawest.com>
2021-06-11 14:28:20 +0300

    created custom endpoint for fetch users events

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,30 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils')
+
+const { default: createStrapi } = require("strapi");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  //  Get Logged in users 
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { massages: [{id: 'No authorization heder was found'}] },
+      ])
+    }
+
+    const data = await strapi.services.events.find({user: user.id});
+
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    return sanitizeEntity(data, {model: strapi.models.events})
+  }
+};
